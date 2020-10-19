82bc7b92f6904a27980747e7e1ff7a2e824da0e9
Susmita Biswas <susmita.biswas006@gmail.com>
2020-10-18 23:44:54 -0400

    modified code to dynamic

--- SimpleCracker.java
+++ SimpleCracker.java
@@ -1,105 +1,140 @@
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleCracker {
-public SimpleCracker() {
+    public SimpleCracker() {
 
-}
-
-//Read the input file
-
-static String[] readFile(String file) throws IOException {
-
-	FileReader fileReader = new FileReader(file);
-	BufferedReader bufferedReader = new BufferedReader(fileReader);
-	List<String> lines = new ArrayList<String>();
-	String line = null;
-	while ((line = bufferedReader.readLine()) != null) {
-		lines.add(line);
-	}
-	bufferedReader.close();
-	return lines.toArray(new String[lines.size()]);
-}
-
-
-//converts a byte array into a String that contains the
-//hexadecimal representation of the byte array
-
-public static String toHex(byte[] bytes) {
-	BigInteger bi = new BigInteger(1, bytes);
-	return String.format("%0" + (bytes.length << 1) + "X", bi);
-}
-
-
-
-public static void main(String[] args) {
-
-
-	// Read shadow file provided , save the Usernames, salt, and
-	//hashes into memory
+    }
+    /**
+     * Subject CS645 Project1
+     * @author Susmita Biswas
+     * @author Ida Jebakirubai
+     */
+       
+    /**
+	 * This method is used to return users list from shadow file
+	 * 
+	 * @param fileName
+	 * @return userList- List of users in shadow file
+	 * @throws IOException
+	 */
 	
-	String[] usersInput = new String[9];
-	String[] saltsInput = new String[9];
-	String[] hashesInput = new String[9];
-	String[] shadowSimplePassword;
-	
-	MessageDigest hashGenerator;
-
-	try {
-		
-		hashGenerator = MessageDigest.getInstance("MD5");
-		
-		shadowSimplePassword = readFile("shadow-simple");
-		
-		for (int i=0; i< shadowSimplePassword.length -1; i++) {
-			usersInput[i] = shadowSimplePassword[i].substring(0, 5);
-			saltsInput[i] = shadowSimplePassword[i].substring(6, 14);
-			hashesInput[i] = shadowSimplePassword[i].substring(15,
-			shadowSimplePassword[i].length());
+	static List<SimpleUser> getUsersFromShadowFile(String fileName) throws IOException {
+		List<SimpleUser> userList = new ArrayList<SimpleUser>();
+		Path path = Paths.get(fileName);
+		/*
+		 * Read all contents from shadow file
+		 */
+		List<String> shadowSimplePassword = Files.readAllLines(path);
+		/*
+		 * Loop through each line and populate user with username,salt and hash
+		 */
+		for (String lines : shadowSimplePassword) {
+			/*
+			 * Split lines based on ":"
+			 * As mentioned, each line will be in format username:salt:hash
+			 */
+			String[] parts = lines.split(":");
+			/*
+			 * Splitting the parts based on ":", 
+			 * username = parts[0]
+			 * salt = parts[1]
+			 * hash = parts[2]
+			 */
+			String userName = parts[0];
+			String salt = parts[1];
+			String hash = parts[2];
+			userList.add(new SimpleUser(userName, salt, hash));
 		}
+		return userList;
+	}
+
+
+    //converts a byte array into a String that contains the hexadecimal representation of the byte array
+
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        return String.format("%0" + (bytes.length << 1) + "X", bi);
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        String shadowFileName = "./shadow-simple";
+		String passwordFileName = "./common-passwords.txt";
+        List<SimpleUser> userList = new ArrayList<SimpleUser>();
+
+        MessageDigest hashGenerator;
+
+        try {
+            hashGenerator = MessageDigest.getInstance("MD5");
+            /*
+			 * Read shadow file into memory and get users from the file
+			 */
+            userList = getUsersFromShadowFile(shadowFileName);
+            /*
+			 * Read dictionary file into memory
+			 */
+			Path path = Paths.get(passwordFileName);
+			List<String> dictionaryPasswords = Files.readAllLines(path);
+			/*
+			 * Check if both users and dictionary list have one data
+			 */
+			if(userList.size() > 0 && dictionaryPasswords.size() > 0) {
+				
+				for(SimpleUser user : userList) {
+					for(String password : dictionaryPasswords) {
+							// Concatenate each dictionary item with each salt
+		                    byte[] saltPasswordInBytes = ((String)(user.salt+  password)).getBytes();
+	
+		                    //Hash the salt password in bytes and converting it to hex using toHex function
+		                    String saltedHashForPassword = toHex(hashGenerator.digest(saltPasswordInBytes));
 	
 	
-		// Read the dictionary file
-		String[] dictionaryPasswords = readFile("common-passwords.txt");
-		for (int i = 0; i < hashesInput.length-1; i++) {
-			for (int j=0; j < dictionaryPasswords.length; j++) {
-			
-				// Concatenate each dictionary item with each salt
-				byte[] saltPasswordInBytes =
-				((String)(saltsInput[i] + dictionaryPasswords[j])).getBytes();
+							if (saltedHashForPassword.compareTo(user.hash) == 0) { 
+								System.out.println(user.userName+ ":" + password); 
+							} 
+							
+						}
+					}
 				
-				//Hash the salt password in bytes and converting
-				//it to hex using toHex function
-				String saltedHashForPassword =
-				toHex(hashGenerator.digest(saltPasswordInBytes));
-				
-				
-				//Print the list of users whose passwords are
-				//compromised from the common password file
-				if (saltedHashForPassword.compareTo(hashesInput[i]) == 0) {
-				System.out.println(usersInput[i] + ":" +
-				dictionaryPasswords[j]);
+				}else {
+					System.out.println("Please check your shadow file or password file"); 
 				}
-			}
-		}
+           
+        } catch (IOException e) {
+            // catch block
+            e.printStackTrace();
+        }
+        catch (NoSuchAlgorithmException e1) {
+            // catch block
+            e1.printStackTrace();
+        }
 
-	} catch (IOException e) {
-		// catch block
-		e.printStackTrace();
-	}
-	catch (NoSuchAlgorithmException e1) {
-		// catch block
-		e1.printStackTrace();
-	}
+        System.exit(0);
+    }
+}
 
-	System.exit(0);
+class SimpleUser {
+	public String userName;
+	public String salt;
+	public String hash;
+
+	SimpleUser(String userName, String salt, String hash) {
+		this.userName = userName;
+		this.salt = salt;
+		this.hash = hash;
 	}
 }
+
+
